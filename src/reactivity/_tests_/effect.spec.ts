21ec152643c1b1f20d5040f014ffb0cbdf7ce127
ly-lym <15827207472@163.com>
2023-06-11 23:52:10 +0800

    add effect scheduler stop onstop

--- src/reactivity/_tests_/effect.spec.ts
+++ src/reactivity/_tests_/effect.spec.ts
@@ -1,5 +1,5 @@
-import {describe, expect, test} from '@jest/globals';
-import {effect} from "../effect"
+import {describe, expect, jest, test} from '@jest/globals';
+import {effect,stop} from "../effect"
 import { reactive } from "../reactive";
 describe('effect',()=>{
     test("path", ()=>{
@@ -26,4 +26,55 @@ describe('effect',()=>{
         expect(foo).toBe(12);
         expect(r).toBe("foo");
     });
+    test("scheduler",() =>{
+        //当expect第一次执行的时候，还会执行fn
+        //当响应式set update 不会执行fn,而是执行scheduler
+        //当执行runner才会再次执行fn
+        
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(()=>{
+            run = runner;
+        })
+        const obj = reactive({foo:1});
+        const runner =effect( () =>{
+            dummy = obj.foo;
+        },{scheduler})
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        expect(dummy).toBe(1);
+        run();
+        expect(dummy).toBe(2);
+    });
+    test("stop", () => {
+        let dummy;
+        const obj = reactive({ prop: 1 });
+        const runner = effect(() => {
+          dummy = obj.prop;
+        });
+        obj.prop = 2;
+        expect(dummy).toBe(2);
+        stop(runner);
+        // obj.prop = 3
+        obj.prop++;
+        expect(dummy).toBe(3);
+    
+        // stopped effect should still be manually callable
+        runner();
+        expect(dummy).toBe(3);
+      });
+      test("onstop", () => {
+        let dummy;
+        const obj = reactive({ prop: 1 });
+        const onStop = jest.fn();
+        const runner = effect(() => {
+          dummy = obj.prop;
+        },{
+            onStop,
+        });
+        stop(runner);
+        expect(onStop).toBeCalledTimes(1);
+      });
 });
--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,12 +1,32 @@
+import { extend } from "../shared";
+
 class ReactiveEffect{
     private _fn:any;
-    constructor(fn){
+    deps =[];
+    active = true;
+    onStop ?:()=>void;
+    constructor(fn: any, public scheduler?){
         this._fn = fn;
     }
     run(){
         activeEffect = this;
         return this._fn();
     }
+    stop(){
+        if(this.active){
+            cleanupEffect(this);
+            if(this.onStop){
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
+}
+
+function cleanupEffect(effect){
+    effect.deps.forEach((dep:any)=>{
+        dep.delete(effect);
+    });
 }
 const targetMap = new Map();
 export function track (target,key){
@@ -20,21 +40,37 @@ export function track (target,key){
         dep = new Set();
         depsMap.set(key,dep);
     }
+    if(!activeEffect)  return;
     dep.add(activeEffect);
-
+    activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key){
     let depsMap = targetMap.get(target);
     let dep = depsMap.get(key);
     for(const effect of dep){
-        effect.run();
+        if(effect.scheduler){
+            effect.scheduler();
+        }else{
+            effect.run();
+        }
     }
 }
 
 let activeEffect;
-export function effect(fn){
-    const _effect = new ReactiveEffect(fn);
+export function effect(fn,options: any={}){
+    const _effect = new ReactiveEffect(fn,options.scheduler);
+    //_effect.onStop = options.onStop;
+    //Object.assign(_effect,options);//TODO
+    extend(_effect,options);
     _effect.run();
-    return _effect.run.bind(_effect);
+
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+
+    return runner;
+}
+
+export function stop(runner){
+    runner.effect.stop();
 }
